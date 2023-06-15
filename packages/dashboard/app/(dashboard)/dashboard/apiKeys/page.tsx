1a5b52d0dd580aa0d4664d317608fff0049c2d60
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-06-16 02:49:00 +0530

    fixed button

--- packages/dashboard/app/(dashboard)/dashboard/apiKeys/page.tsx
+++ packages/dashboard/app/(dashboard)/dashboard/apiKeys/page.tsx
@@ -1,5 +1,6 @@
 import ApiKeyList from '@/components/Lists/ApiKeyList'
 import Button from '@/components/ui/Button'
+import { Plus } from 'lucide-react'
 import React from 'react'
 
 function Page() {
@@ -8,8 +9,8 @@ function Page() {
         <h1 className='text-4xl'>
             Your Api Keys
         </h1>
-        <div className="flex flex-col w-full gap-4">
-          <Button>Add</Button>
+        <div className="flex flex-col gap-4">
+          <Button className='w-min text-lg px-4 flex items-center gap-2'>Add <Plus /></Button>
           <ApiKeyList />
         </div>
 
