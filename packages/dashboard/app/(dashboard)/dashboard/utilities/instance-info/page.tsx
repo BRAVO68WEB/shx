b734709cb0419f753144e9943864c720edc62249
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-06-17 02:07:32 +0530

    setup utilitites

---
+++ packages/dashboard/app/(dashboard)/dashboard/utilities/instance-info/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <div>Page Instance</div>
+  )
+}
+
+export default Page
---
+++ packages/dashboard/app/(dashboard)/dashboard/utilities/settings/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <div>Page Settings</div>
+  )
+}
+
+export default Page
