41f676948b231d6f9fe7a3e9614b9b42c213678b
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-06-17 02:11:56 +0530

    setup instance info

--- packages/dashboard/app/(dashboard)/dashboard/utilities/instance-info/page.tsx
+++ packages/dashboard/app/(dashboard)/dashboard/utilities/instance-info/page.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from 'react';
 
 function Page() {
-  return (
-    <div>Page Instance</div>
-  )
+	const urls = [
+		{
+			id: 'afdas',
+			originalURL: 'https://www.google.com',
+			shortenedURL: 'https://www.google.com',
+		},
+	];
+
+	return (
+		<div>
+			<table className="min-w-full divide-y divide-gray-700">
+				<thead className="p-2">
+					<tr>
+						<th
+							scope="col"
+							className="py-3.5 pl-4 pr-5 text-left text-lg font-semibold text-white"
+						>
+							Original URL
+						</th>
+						<th
+							scope="col"
+							className="py-3.5 pl-4 pr-3 w-full text-left text-lg font-semibold text-white"
+						>
+							Shortened URL
+						</th>
+					</tr>
+				</thead>
+				<tbody className="divide-y p-2">
+					{urls.map(({ originalURL, shortenedURL, id }) => (
+						<tr className="bg-gray-900 rounded" key={id}>
+							<td className="whitespace-nowrap  pl-4 pr-20 text-sm font-medium text-white">
+								<div className="flex items-center gap-3">
+									{originalURL}
+									
+								</div>
+							</td>
+							<td className="whitespace-nowrap  pl-4 text-sm font-medium text-white">
+								<div className="flex items-center gap-3">
+									{shortenedURL}
+									
+								</div>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
 }
 
-export default Page
+export default Page;
--- packages/dashboard/app/(dashboard)/dashboard/utilities/settings/page.tsx
+++ packages/dashboard/app/(dashboard)/dashboard/utilities/settings/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Page() {
   return (
-    <div>Page Settings</div>
+    <div>
+        
+    </div>
   )
 }
 
