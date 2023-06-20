20542af11bb77941dce6769cd89f03a7f05459ca
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-06-20 23:04:58 +0530

    updated ui to use maps

--- packages/dashboard/app/(dashboard)/dashboard/utilities/download-config/page.tsx
+++ packages/dashboard/app/(dashboard)/dashboard/utilities/download-config/page.tsx
@@ -2,36 +2,41 @@ import Button from '@/components/ui/Button';
 import { Download } from 'lucide-react';
 import React from 'react';
 
+const configs = [
+	{
+		name: 'Lorem Ipsum Config',
+		id: '1',
+	},
+	{
+		name: 'Lorem Cnofig',
+		id: '12',
+	},
+	{
+		name: 'Lorem Config Config',
+		id: '13',
+	},
+	{
+		name: 'Lorem ',
+		id: '14',
+	},
+];
+
 function Page() {
 	return (
 		<>
-			<div className="w-full my-3 p-4 flex items-center justify-between bg-gray-900 rounded">
-				<p>Lorem Ipsum Config</p>
-				<Button size={'icon'} variant={'transparent'}>
-					<Download />
-				</Button>
-			</div>
-
-			<div className="w-full my-3 p-4 flex items-center justify-between bg-gray-900 rounded">
-				<p>Lorem Ipsum Config</p>
-				<Button size={'icon'} variant={'transparent'}>
-					<Download />
-				</Button>
-			</div>
-
-			<div className="w-full my-3 p-4 flex items-center justify-between bg-gray-900 rounded">
-				<p>Lorem Ipsum Config</p>
-				<Button size={'icon'} variant={'transparent'}>
-					<Download />
-				</Button>
-			</div>
-
-			<div className="w-full my-3 p-4 flex items-center justify-between bg-gray-900 rounded">
-				<p>Lorem Ipsum Config</p>
-				<Button size={'icon'} variant={'transparent'}>
-					<Download />
-				</Button>
-			</div>
+			{configs.map(({ name, id }) => {
+				return (
+					<div
+						key={id}
+						className="w-full my-3 p-4 flex items-center justify-between bg-gray-900 rounded"
+					>
+						<p>{name}</p>
+						<Button size={'icon'} variant={'transparent'}>
+							<Download />
+						</Button>
+					</div>
+				);
+			})}
 		</>
 	);
 }
