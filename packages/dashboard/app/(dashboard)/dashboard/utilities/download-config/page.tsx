3b5885ebd9e6d6f10e7eb9397c4b13bff8f9e849
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-06-20 23:02:52 +0530

    finished config download ui

--- packages/dashboard/app/(dashboard)/dashboard/utilities/download-config/page.tsx
+++ packages/dashboard/app/(dashboard)/dashboard/utilities/download-config/page.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import Button from '@/components/ui/Button';
+import { Download } from 'lucide-react';
+import React from 'react';
 
 function Page() {
-  return <div>Page</div>;
+	return (
+		<>
+			<div className="w-full my-3 p-4 flex items-center justify-between bg-gray-900 rounded">
+				<p>Lorem Ipsum Config</p>
+				<Button size={'icon'} variant={'transparent'}>
+					<Download />
+				</Button>
+			</div>
+
+			<div className="w-full my-3 p-4 flex items-center justify-between bg-gray-900 rounded">
+				<p>Lorem Ipsum Config</p>
+				<Button size={'icon'} variant={'transparent'}>
+					<Download />
+				</Button>
+			</div>
+
+			<div className="w-full my-3 p-4 flex items-center justify-between bg-gray-900 rounded">
+				<p>Lorem Ipsum Config</p>
+				<Button size={'icon'} variant={'transparent'}>
+					<Download />
+				</Button>
+			</div>
+
+			<div className="w-full my-3 p-4 flex items-center justify-between bg-gray-900 rounded">
+				<p>Lorem Ipsum Config</p>
+				<Button size={'icon'} variant={'transparent'}>
+					<Download />
+				</Button>
+			</div>
+		</>
+	);
 }
 
 export default Page;
