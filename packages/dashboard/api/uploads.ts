da082c6839f6da2120472eea01cb500ffa39e1dd
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-06-21 22:44:15 +0530

    removed unnecessary catch block

--- packages/dashboard/api/uploads.ts
+++ packages/dashboard/api/uploads.ts
@@ -2,17 +2,13 @@ import { Axios } from 'axios';
 
 export class Uploads {
 	axios: Axios;
-	constructor(axios:Axios) {
-		this.axios = axios
+	constructor(axios: Axios) {
+		this.axios = axios;
 	}
 	async getAllUploads() {
-		try {
-			const res = await this.axios.get('/upload');
-			const data = res.data as IFile[];
-			return data;
-		} catch (e) {
-			throw e
-		}
+		const res = await this.axios.get('/upload');
+		const data = res.data as IFile[];
+		return data;
 	}
 }
 
