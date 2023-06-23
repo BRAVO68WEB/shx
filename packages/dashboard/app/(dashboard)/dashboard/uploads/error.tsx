82220725ef6ba3863995f9357af7e5f55a8e4bc4
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-06-23 22:33:47 +0530

    removed unused logs and variables

--- packages/dashboard/app/(dashboard)/dashboard/uploads/error.tsx
+++ packages/dashboard/app/(dashboard)/dashboard/uploads/error.tsx
@@ -1,12 +1,6 @@
 'use client';
 
-import Cookies from 'js-cookie';
-import { useEffect } from 'react';
-
 function ErrorPage() {
-	useEffect(() => {
-		console.log(Cookies.get());
-	},[]);
 	return <div>ErrorPage</div>;
 }
 
