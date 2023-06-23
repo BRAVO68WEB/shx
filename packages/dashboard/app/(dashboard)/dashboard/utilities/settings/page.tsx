cb107ef4c9f77ace5e7637ca3664c0c75a362601
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-06-23 14:38:05 +0530

    integrated get settings

--- packages/dashboard/app/(dashboard)/dashboard/utilities/settings/page.tsx
+++ packages/dashboard/app/(dashboard)/dashboard/utilities/settings/page.tsx
@@ -1,27 +1,44 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '@/components/ui/Button';
 import Input from '@/components/ui/Input';
 import TagInput from '@/components/TagInput';
 import api from '@/api';
 
 function Page() {
-	const [imageExts, setImageExts] = useState<string[]>([]);
-	const [fileExts, setFileExts] = useState<string[]>([]);
+	const [settings, setSettings] = useState<ISettings>({
+		imageExtensions: [],
+		fileExtensions: [],
+		theme: '',
+		language: '',
+	});
 
 	const addImageExt = (tag: string) => {
-		setImageExts(old => {
-			return [...old, tag];
+		setSettings(old => {
+			return {
+				...old,
+				imageExtensions: [...old.imageExtensions, tag],
+			};
 		});
 	};
 	const addFileExt = (tag: string) => {
-		setFileExts(old => {
-			return [...old, tag];
+		setSettings(old => {
+			return {
+				...old,
+				fileExtensions: [...old.fileExtensions, tag],
+			};
 		});
 	};
 
-	
+	const getSettings = async () => {
+		const res = await api.settings.getCurrentSettings();
+		setSettings(res)
+	};
+
+	useEffect(() => {
+		getSettings();
+	}, []);
 
 	return (
 		<div className="flex flex-col gap-8 w-full mt-10">
@@ -37,6 +54,7 @@ function Page() {
 					name="theme"
 					className="mt-2 block w-full bg-transparent rounded-md py-1.5 pl-3 pr-10 text-white sm:text-sm sm:leading-6 border-primary border"
 					defaultValue="dark"
+					value={settings.theme}
 				>
 					<option value={'dark'}>Dark</option>
 				</select>
@@ -53,6 +71,7 @@ function Page() {
 					name="Language"
 					className="mt-2 bg-transparent block w-full rounded-md py-1.5 pl-3 pr-10 text-white sm:text-sm sm:leading-6 border border-primary"
 					defaultValue="en"
+					value={settings.language}
 				>
 					<option value={'en'}>English</option>
 				</select>
@@ -67,12 +86,12 @@ function Page() {
 				/>
 			</div>
 			<TagInput
-				tags={imageExts}
+				tags={settings.imageExtensions}
 				onAddTags={addImageExt}
 				placeholder="Image Extensions"
 			/>
 			<TagInput
-				tags={fileExts}
+				tags={settings.fileExtensions}
 				onAddTags={addFileExt}
 				placeholder="File Extensions"
 			/>
