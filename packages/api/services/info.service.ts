f5c6c2fd4d1db80c7d4912791900e65dfa2c5b78
Jyotirmoy Bandyopadhyaya [Bravo68] <jbandyopadhayaya@gmail.com>
2023-06-24 13:28:58 +0530

    ⚙ modified sys info api

--- packages/api/services/info.service.ts
+++ packages/api/services/info.service.ts
@@ -1,3 +1,5 @@
+import os from 'node:os';
+
 export default class InfoService {
 	static async getSystemInfo() {
 		const sys = {
@@ -7,19 +9,8 @@ export default class InfoService {
 			uptime: process.uptime(),
 			memoryUsage: process.memoryUsage(),
 			cpuUsage: process.cpuUsage(),
-			pid: process.pid,
-			ppid: process.ppid,
-			cwd: process.cwd(),
-			execPath: process.execPath,
-			execArgv: process.execArgv,
-			argv: process.argv,
-			env: process.env,
-			config: process.config,
-			title: process.title,
-			version: process.version,
-			versions: process.versions,
-			release: process.release,
-			features: process.features,
+			kernelVersion: os.release(),
+			hostname: os.hostname(),
 		};
 
 		return sys;
