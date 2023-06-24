13aa51dc9cd2914b57e2b0a3e47cfc0c8fcf4fda
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-06-24 20:57:55 +0530

    added note page and passkey validation

--- packages/dashboard/api/notes.ts
+++ packages/dashboard/api/notes.ts
@@ -6,24 +6,25 @@ export class Notes {
 	constructor(axios: Axios) {
 		this.axios = axios;
 	}
-	async getAllNotes(search?:string) {
-		const res = await this.axios.get('/gist',{params:{search}});
+	async getAllNotes(search?: string) {
+		const res = await this.axios.get('/gist', { params: { search } });
 		const data = res.data.data as INote[];
 		return data;
 	}
+	async getSingleNote(id: string, passkey: string = '') {
+		const res = await this.axios.get(`/gist/${id}`, { params: { passkey } });
+		const data = res.data.data as INote;
+		return data;
+	}
 	async uploadSingleNote(data: AddNoteType) {
-        console.log("uploading")
-        if(!data.passkey){
-            delete data.passkey
-        }
+		console.log('uploading');
+		if (!data.passkey) {
+			delete data.passkey;
+		}
 		const res = await this.axios.post('/gist', data);
 		return res.data.data as INote;
 	}
-	async deleteSingleNote({
-		noteID,
-	}: {
-		noteID: string;
-	}) {
+	async deleteSingleNote({ noteID }: { noteID: string }) {
 		const res = await this.axios.delete(`/gist/${noteID}`);
 		return res;
 	}
